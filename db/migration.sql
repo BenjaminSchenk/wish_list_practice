067661f98fffaf3a70767da537bd00dbf1eb252f
BenjaminSchenk <benjamin.k.schenk@gmail.com>
2023-06-15 11:18:28 -0700

    Update migration.sql

--- db/migration.sql
+++ db/migration.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS wishes
+DROP TABLE IF EXISTS wishes;
 
-CREATE TABLE wishes ({
+CREATE TABLE wishes (
     id serial PRIMARY KEY,
     name varchar(50),
     price numeric(9, 2)
-})
+);
 
 INSERT INTO wishes (name, price) VALUES
-('Remade Mortal Kombat 1', 70.00)
-('Couch', 2500.72)
-('Mac computer', 1700.36)
+('Remade Mortal Kombat 1', 70.00),
+('Couch', 2500.72),
+('Mac computer', 1700.36);
